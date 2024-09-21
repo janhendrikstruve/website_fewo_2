cd5000d3e6f701d43d297b3ebb611e1af06e56c6
jan-hendrik <j.struve@posteo.de>
2024-09-21 13:29:23 +0200

    Finished boring hero page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,62 @@
 <template>
-  <div class="bg-white">
+  <div class="bg-[#F0ECE3]">
     <header class="absolute inset-x-0 top-0 z-50">
-      <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        class="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="../assets/moewe.svg" alt="" />
+            <img class="h-12 w-auto" src="../assets/moewe.svg" alt="" />
           </a>
         </div>
         <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <button
+            type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = true"
+          >
             <span class="sr-only">Open main menu</span>
             <Bars3Icon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
+          <a
+            v-for="item in navigation"
+            :key="item.name"
+            :href="item.href"
+            class="text-sm font-semibold leading-6 text-gray-900"
+            >{{ item.name }}</a
+          >
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
           <!-- Leerer Platzhalter anstatt login -->
         </div>
       </nav>
-      <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <Dialog
+        class="lg:hidden"
+        @close="mobileMenuOpen = false"
+        :open="mobileMenuOpen"
+      >
         <div class="fixed inset-0 z-50" />
-        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              <img
+                class="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
             </a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <button
+              type="button"
+              class="-m-2.5 rounded-md p-2.5 text-gray-700"
+              @click="mobileMenuOpen = false"
+            >
               <span class="sr-only">Close menu</span>
               <XMarkIcon class="h-6 w-6" aria-hidden="true" />
             </button>
@@ -37,7 +64,13 @@
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >{{ item.name }}</a
+                >
               </div>
             </div>
           </div>
@@ -46,36 +79,132 @@
     </header>
 
     <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      <div
+        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
       </div>
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Willkommen an der Nordsee 🌊</h1>
-          <img class="h-auto w-auto mt-8 rounded-xl" src="../assets/ansicht_haus_hinten.jpg" alt="Ansicht hinten" />
-          <p class="mt-6 text-lg leading-8 text-gray-600">Genießen sie Ihre Ferien am Deich, mal fernab von allem Trubel und ohne Nachbarn. Entspannen Sin in unserem Großzügigen und barrierefreien Ferienhaus in einmaliger Alleinlage mit Topausstattung. Vom Deutschen Tourismus mit 5 Sternen ausgezeichnet. Strand nur 2500 m entfernt.</p>
+          <h1
+            class="text-4xl font-bold tracking-tight text-[#596E79] sm:text-6xl"
+          >
+            Willkommen bei <br />
+            Opa Hans
+          </h1>
+          <img
+            class="h-auto w-auto mt-8 rounded-xl"
+            src="../assets/ansicht_haus_hinten.jpg"
+            alt="Ansicht hinten"
+          />
+        </div>
+        <!-- Abschnitt Bullet Points -->
+        <div
+          class="grid items-center max-w-4xl grid-cols-2 gap-4 mx-auto md:mt-20 md:grid-cols-4 mt-8"
+        >
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center col-span-2 border rounded-md"
+          >
+            ⭐ Fünf Sterne vom Tourismusverband
+          </div>
+
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center col-span-2 border rounded-md"
+          >
+            🌊 Badestelle nur 2,5 KM entfernt
+          </div>
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center border rounded-md"
+          >
+            🌸 Ferien am Deich
+          </div>
+
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center border rounded-md"
+          >
+            🌅 Alleinlage
+          </div>
+
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center border rounded-md"
+          >
+            Barrierefrei
+          </div>
+
+          <div
+            class="bg-white h-12 flex shadow-lg items-center justify-center border rounded-md"
+          >
+            ✨ Top Ausstattung
+          </div>
         </div>
       </div>
-      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
-import HomeView from './HomeView.vue'
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+import HomeView from "./HomeView.vue";
 
 const navigation = [
-  { name: 'Ferienhaus', href: '#' },
-  { name: 'Bauernhof', href: '#' },
-  { name: 'Umgebung', href: '#' },
-  { name: 'Prise und Buchung', href: '#' },
-  { name: 'Kontakt', href: '#' },
-]
+  { name: "Ferienhaus", href: "#" },
+  { name: "Umgebung", href: "#" },
+  { name: "Prise und Buchung", href: "#" },
+  { name: "Kontakt", href: "#" },
+];
 
-const mobileMenuOpen = ref(false)
+const mobileMenuOpen = ref(false);
 </script>
