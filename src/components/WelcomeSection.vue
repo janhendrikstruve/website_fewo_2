176da39a64109bdf67387ba29c4861eb2471d413
henne <git@wernersen.mozmail.com>
2025-08-31 18:53:57 +0700

    Refactoring Button welcome page huebsch gemacht

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -15,7 +15,7 @@
       </p>
       <button
         type="button"
-        class="mt-4 self-center text-white bg-[#405D72] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-8 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="mt-4 self-center text-white bg-[#405D72] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-8 py-2.5 text-center inline-flex items-center"
       >
         Erfahre mehr
         <svg
