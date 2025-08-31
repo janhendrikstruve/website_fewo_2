4b37f10af352e3d5f786b3df62c46bfc29ed052d
henne <git@wernersen.mozmail.com>
2025-08-31 18:52:24 +0700

    Refactoring Pictures auf picture Page gedoppelt

--- src/components/Pictures.vue
+++ src/components/Pictures.vue
@@ -2,6 +2,39 @@
   <section
     class="flex flex-col items-center justify-center overflow-hidden relative h-screen"
   >
+  <h2>Innen:</h2>
+    <div
+      class="flex transition-transform duration-500 ease-in-out"
+      :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >
+      
+      <img
+        v-for="(img, index) in images"
+        :key="index"
+        :src="img.src"
+        :alt="img.alt"
+        class="min-w-full object-contain"
+      />
+    </div>
+
+    <!-- Punkte-Navigation -->
+    <div class="mt-4 flex justify-center gap-2">
+      <button
+        v-for="(img, index) in images"
+        :key="'dot-' + index"
+        @click="currentIndex = index"
+        :class="[
+          'w-3 h-3 rounded-full',
+          currentIndex === index ? 'bg-black' : 'bg-gray-400',
+        ]"
+      ></button>
+    </div>
+    <h2 class="mt-10 ">
+      Außen:
+    </h2>
     <div
       class="flex transition-transform duration-500 ease-in-out"
       :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
